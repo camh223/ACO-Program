afefc1cfa93452a6279f643e736a86be75cc0352
camh223 <camhallett2204@gmail.com>
2023-12-14 19:40:16 +0000

    Created generate heuristic function
    Create a function to generate heuristic matrix.

--- aco_program.py
+++ aco_program.py
@@ -1,6 +1,18 @@
 import dist_graph, pheromone
 
+
+def gen_heuristic(d_size, d):
+    # Initialise heuristic matrix with zeroes.
+    H = [[0 for i in range(d_size)] for j in range(d_size)]
+    for i in range(d_size):
+        for j in range(d_size):
+            if i != j:
+                H[i][j] = 1/d[i][j]
+    return H
+
+
 if __name__ == '__main__':
     d_size, d = dist_graph.build_graph('burma14.xml')
     phero_map = pheromone.phero_init(d_size)
-    print(phero_map)
+    H = gen_heuristic(d_size, d)
+    print(H)
