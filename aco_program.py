71cc9111103bd2af21b7842fa25beb79bf21345c
camh223 <camhallett2204@gmail.com>
2023-12-14 16:22:01 +0000

    Added value for number of ants

--- aco_program.py
+++ aco_program.py
@@ -1,6 +1,7 @@
 import dist_graph, pheromone
 
 if __name__ == '__main__':
+    num_ants = 5
     d_size, d = dist_graph.build_graph('burma14.xml')
     phero_map = pheromone.phero_init(d_size)
     print(phero_map)
