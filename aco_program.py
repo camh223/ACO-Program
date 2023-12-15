c5a1d6e93e93d634d24abf986c4b3b97c239870a
camh223 <camhallett2204@gmail.com>
2023-12-15 21:57:09 +0000

    Commenting complete

--- aco_program.py
+++ aco_program.py
@@ -2,16 +2,34 @@ import dist_graph, pheromone, ant_movement
 
 
 def init_heuristic(d_size, d):
+    """
+    A function to initialise the heuristic matrix based on 1/d[i][j]
+    :param d_size: The number of cities
+    :param d: The distance matrix containing the weight for each edge
+    :return: H - The heuristic matrix
+    """
     # Initialise heuristic matrix with zeroes.
     H = [[0 for i in range(d_size)] for j in range(d_size)]
     for i in range(d_size):
         for j in range(d_size):
+            # Probability of travelling to current node is always 0
             if i != j:
                 H[i][j] = 1/d[i][j]
     return H
 
 
 def run_aco(num_ants, alpha, beta, e, Q, max_runs, file_name):
+    """
+    A function to start running the ant colony optimisation functionality.
+    :param num_ants: The number of ants in each run of the program
+    :param alpha: A fixed parameter that determines the influence of the pheromone
+    :param beta: A fixed parameter that determines the influence of the heuristic matrix
+    :param e: The rate of evaporation
+    :param Q: A fixed parameter that determines the influence of fitness on the pheromone
+    :param max_runs: The number of times the algorithm will run
+    :param file_name: The name of the file containing the dataset to perform ACO on
+    :return: best_fit - the most optimal solution cost
+    """
     runs = 0
     best_fit = -1
     d_size, d = dist_graph.build_graph(file_name)
